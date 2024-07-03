c628d5874658a8778fe334b809ed1febe5fe0c4a
Stuart Sessions <ssessions@veracode.com>
2024-07-03 09:43:35 -0400

    ThemeData included

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const Color veracodeBlack= Color.fromARGB(255, 36, 38, 38);
+const Color veracodeBlue =  Color.fromARGB(255, 0, 179, 230);
+const Color veracodeWhite = Color.fromARGB(255,244, 244, 244);
+
 void main(){
   runApp( const MyApp() );
 }
@@ -11,11 +15,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Blab-a-Gag',
+      /*theme: ThemeData(
+        appBarTheme: ,
+        navigationBarTheme: ,
+        colorScheme: ColorSche
+      ),*/
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text('Hello World')
-        ,),
+          backgroundColor: veracodeBlack,
+          title: const Text('hello world', 
+            style: TextStyle(
+              fontSize: 40,
+              fontFamily: 'Lato',
+              color:veracodeWhite),),
+        ),
         body: const SizedBox(
           child: Center(
             child: Padding(
