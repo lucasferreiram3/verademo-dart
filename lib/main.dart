42864444c2a2c8b385622b9800f6a60453920e50
Stuart Sessions <ssessions@veracode.com>
2024-07-08 09:07:42 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+// Define colors to be used
 const Color veracodeBlack= Color.fromARGB(255, 36, 38, 38);
 const Color veracodeBlue =  Color.fromARGB(255, 0, 179, 230);
 const Color veracodeWhite = Color.fromARGB(255,244, 244, 244);
+
+//Create themes
+AppBar header = AppBar(
+  title: Text("hello", style: GoogleFonts.lato(),)/*page name variable here*/,
+  backgroundColor: veracodeBlack,
+  actions: <Widget>[
+    IconButton(
+      icon: const Icon(
+        Icons.settings,
+        color: veracodeWhite,
+      ),
+      onPressed: (){print(veracodeWhite);} /*run reset controller,*/
+      )
+  ],
+); 
 
 void main(){
   runApp( const MyApp() );
@@ -22,14 +39,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorSche
       ),*/
       home: Scaffold(
-        appBar: AppBar(
+        appBar: header,
+        /*AppBar(
           backgroundColor: veracodeBlack,
-          title: const Text('hello world', 
-            style: TextStyle(
-              fontSize: 40,
-              fontFamily: 'Lato',
-              color:veracodeWhite),),
+          title: Text('hello world',
+            style: GoogleFonts.lato(
+              textStyle: const TextStyle(color: Colors.blue, fontSize: 30),
+            )),
+
         ),
+        */
         body: const SizedBox(
           child: Center(
             child: Padding(
