bce921c99df259a83c58ab521d19073f4eaf076d
clydeshtino <clydeshtino@gmail.com>
2024-07-10 16:28:37 -0400

    In progress logout

--- lib/pages/logout.dart
+++ lib/pages/logout.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:verademo_dart/utils/constants.dart';
 
 class LogoutPage extends StatelessWidget {
   const LogoutPage({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Logout Screen'),
+    return Dialog(
+      child: Container(
+        decoration: BoxDecoration(
+          color : VConstants.darkNeutral1,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child : Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+              // ignore: prefer_const_constructors
+              Text('Would you like to logout?\n',
+              //textAlign: TextAlign.start,
+              // ignore: prefer_const_constructors
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold, 
+              ),
+              ),
+              // spacing purposes
+              // ignore: prefer_const_constructors
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffEC5B5B),
+                    ),
+                    onPressed: () {}, // TODO: Implement LogoutController trigger event
+                    child: const Text(
+                      'Yes, Log me out!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ],
+              ), 
+          ],
+        ),
+      ),
     );
   }
 }
