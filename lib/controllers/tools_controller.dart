7cb0250cf77cd64be890c8aa7a3b204e1af2da63
clydeshtino <clydeshtino@gmail.com>
2024-07-11 14:48:22 -0400

    Laid out template, designing functions

---
+++ lib/controllers/tools_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ToolsController {
+
+  Future<void> processTools() async {
+
+  }
+  Future<void> ping(host) async {
+
+  }
+  Future<void> fortune(file) async {
+    
+  }
+
+
+}
