576cb4edaf97b0398928bd47b5ee2fa2135e571f
clydeshtino <clydeshtino@gmail.com>
2024-07-11 08:32:07 -0400

    Current Logout Screen

--- lib/pages/logout.dart
+++ lib/pages/logout.dart
@@ -8,38 +8,45 @@ class LogoutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
       child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32,
+          vertical: 16,
+        ),
         decoration: BoxDecoration(
-          color : VConstants.darkNeutral1,
+          color : VConstants.veracodeBlack,
           borderRadius: BorderRadius.circular(10),
         ),
         child : Column(
           mainAxisSize: MainAxisSize.min,
           children: [
               // ignore: prefer_const_constructors
-              Text('Would you like to logout?\n',
+              Text('Are You Sure?\n',
               textAlign: TextAlign.center,
               // ignore: prefer_const_constructors
               style: TextStyle(
                 color: VConstants.veracodeWhite,
-                fontSize: 28,
+                fontSize: 24,
                 fontWeight: FontWeight.bold, 
               ),
               ),
               // spacing purposes
               // ignore: prefer_const_constructors
-              
               Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xffEC5B5B),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
                     ),
                     onPressed: () {}, // TODO: Implement LogoutController trigger event
                     child: const Text(
                       'Yes, Log me out!',
                       style: TextStyle(
-                        color: Colors.white,
+                        color: VConstants.veracodeWhite,
                         fontSize: 14,
                       ),
                     ),
@@ -47,8 +54,12 @@ class LogoutPage extends StatelessWidget {
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       backgroundColor: VConstants.veracodeBlue,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
                     ),
-                    onPressed: () {}, // TODO: Implement LogoutController trigger event
+                    onPressed: () {}, // TODO: Implement redirect to home trigger event
                     child: const Text(
                       'No, More Blab!',
                       style: TextStyle(
