0da1d1b184a6204acc30a5a9057afae4c8b2cecd
clydeshtino <clydeshtino@gmail.com>
2024-07-15 08:36:58 -0400

    Tools Controller (needs testing/call on onPressed)

--- lib/controllers/tools_controller.dart
+++ lib/controllers/tools_controller.dart
@@ -6,7 +6,7 @@ import 'dart:io';
 
 class ToolsController {
   Future<void> processTools() async {
-    File file = new File("fortuneFile"); // May or may not need to change depending on fortunefile for dart
+    File file = File("fortuneFile"); // May or may not need to change depending on fortunefile for dart
     String host = Platform.localHostname;
     String fortuneFile = basename(file.path); 
   }
@@ -15,9 +15,9 @@ class ToolsController {
 
     try {
       try {
-      var proc = await Process.run('grep', ['bash', '-c', 'ping -c1 '] + host).timeout(const Duration(seconds: 5));
-      output = proc.stdout;
-
+        var proc = await Process.run('grep', ['bash', '-c', 'ping -c1 '] + host).timeout(const Duration(seconds: 5));
+        output = proc.stdout;
+        
       } on TimeoutException {
         output = "Ping has timed out!"; // Placeholder will replace with actual handling later
       }
@@ -35,8 +35,12 @@ class ToolsController {
         var proc = await Process.run('grep', ['bash', '-c', cmd]).timeout(const Duration(seconds:5));
         output = proc.stdout;
 
-      } on TimeoutException {}
-    } on Exception {}
+      } on TimeoutException {
+        output = "Fortune has timed out!"; // Placeholder will replace with actual handling later
+      }
+    } on Exception {
+      output = "Error occured intiating fortune"; // Placeholder will replace with actual handling later
+    }
 
     return output;
   }
