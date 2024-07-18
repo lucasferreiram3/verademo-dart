659819d3687219cca7d5a2c4eb7fc390986625b9
kevliu43 <kevliu43@gmail.com>
2024-07-18 11:23:00 -0400

    changed tokens to be in line with new api

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -12,4 +12,27 @@ class ProfileController {
   final username = TextEditingController();
   GlobalKey<FormState> profileFormKey = GlobalKey<FormState>();
 
+  void setVariablesFromUsername(String username) async {
+    try {
+      // Build API call for getProfileInfo
+      print("Building API call to /users/getProfileInfo/");
+      const url = "${VConstants.apiUrl}/users/getProfileInfo/";
+      final uri = Uri.parse(url);
+      final body = jsonEncode(<String, String> {
+        "username": this.username.text,
+      });
+      final Map<String, String> headers = {
+        "content-type": "application/json",
+        "Authorization": VSharedPrefs().token ?? "",
+      };
+
+      // Execute API call for getProfileInfo
+      final response = await http.post(uri, body: body, headers: headers);
+    } catch (err) {
+      print(err);
+    }
+
+
+  }
+
 }
