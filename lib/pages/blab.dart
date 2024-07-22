57d25cf66ff06bf24390354ddbd88b84b441cec5
kevliu43 <kevliu43@gmail.com>
2024-07-22 13:20:57 -0400

    turned appbar into widget

--- lib/pages/blab.dart
+++ lib/pages/blab.dart
@@ -1,12 +1,15 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:verademo_dart/widgets/app_bar.dart';
 
 class BlabPage extends StatelessWidget {
   const BlabPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      appBar: VAppBar('Blab'),
+    );
   }
 }
