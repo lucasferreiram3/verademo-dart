d8435afaa075dcfc385919b701e1861e77def539
kevliu43 <kevliu43@gmail.com>
2024-07-22 15:07:37 -0400

    blabs inp

--- lib/pages/blab.dart
+++ lib/pages/blab.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:verademo_dart/model/Blab.dart';
+import 'package:verademo_dart/utils/constants.dart';
 import 'package:verademo_dart/widgets/app_bar.dart';
 
 class BlabPage extends StatelessWidget {
-  const BlabPage({super.key});
+  const BlabPage(this.blab, {super.key});
+
+  final Blab blab;
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: VAppBar('Blab'),
+      body: SingleChildScrollView(
+        padding: VConstants.pagePadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // TODO: Original post
+          // TODO: Comments list
+          
 
+          // TODO: Add comment field
+        )
+      )
     );
   }
 }
